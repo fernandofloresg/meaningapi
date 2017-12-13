bc4b4b973328dfd74736cf27462d3d69ad15bb4d
fernandofloresg <fflores10@uabc.edu.mx>
2017-12-13 16:54:56 -0700

    añadi los modelos de las respuestas y alumnos

--- meaningapi/api/models.py
+++ meaningapi/api/models.py
@@ -9,3 +9,19 @@ class sentimentCard(models.Model):
 
 	def __str__(self):
 	    return self.title
+
+class student(models.Model):
+	student_id = models.CharField(primary_key=True, max_length=200)
+	name = models.CharField(max_length=200)
+
+	def __str__(self):
+	    return self.name
+
+class sentimentCard(models.Model):
+	text = models.CharField(max_length=200)
+	score_tag = models.CharField(max_length=10)
+	student_id = models.ForeignKey(student, on_delete =models.CASCADE)
+	sentiment_card_id = models.ForeignKey(sentimentCard, on_delete =models.CASCADE)
+
+	def __str__(self):
+	    return self.title
